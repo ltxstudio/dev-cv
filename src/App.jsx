9b8241e20c838cfe152a38c86a8c8f2e15524174
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-25 20:51:37 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
+import Summary from './components/Summary';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="max-w-screen-lg mx-auto p-4">
+    <div className="font-sans bg-gray-50 text-gray-900">
       <Header />
+      <Summary />
       <Experience />
       <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 };
